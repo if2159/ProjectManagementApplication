ec9f2e46bd7365fa93c42cc8218b474f067d4094
MikeZBi <mikezengbi@gmail.com>
2018-03-19 19:43:59 -0700

    Added additional attributes to Departments Table to specify location

--- SQL/CREATE DEPARTMENTS.sql
+++ SQL/CREATE DEPARTMENTS.sql
@@ -1,7 +1,13 @@
 CREATE TABLE DEPARTMENTS (
   ID       INT          NOT NULL IDENTITY (1, 1) PRIMARY KEY,
   NAME     VARCHAR(70)  NOT NULL,
-  Location VARCHAR(250) NOT NULL,
+  Street_Number VARCHAR(20) NOT NULL,
+  Street_Name VARCHAR(50) NOT NULL,
+  City VARCHAR(60) NOT NULL,
+  State_Province_Region VARCHAR(50) NOT NULL,
+  ZipCode VARCHAR(16) NOT NULL,
+  Country VARCHAR(90) NOT NULL,
+  LOCATION AS (Street_Number + ' ' + Street_Name + ' ' + City + ', ' + State_Province_Region + ' ' + ZipCode + ' ' + Country),
   UNIQUE (NAME),
   NumberOfEmployees              AS (dbo.GetNumberOfEmployees(ID))
 );
