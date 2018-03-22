364c209a0a734c63d31bacda7f11b81ef0b94d0c
Ian Fennen <ifennen@YMAIL.COM>
2018-03-21 23:46:11 -0500

    Closed issues #1 and #4.
    Signed-off-by: Ian Fennen <ifennen@YMAIL.COM>

--- SQL/CREATE DEPARTMENTS.sql
+++ SQL/CREATE DEPARTMENTS.sql
@@ -1,13 +1,12 @@
 CREATE TABLE DEPARTMENTS (
-  ID       INT          NOT NULL IDENTITY (1, 1) PRIMARY KEY,
+  DEPARTMENT_ID       INT          NOT NULL IDENTITY (1, 1) PRIMARY KEY,
   NAME     VARCHAR(70)  NOT NULL,
-  Street_Number VARCHAR(20) NOT NULL,
-  Street_Name VARCHAR(50) NOT NULL,
-  City VARCHAR(60) NOT NULL,
-  State_Province_Region VARCHAR(50) NOT NULL,
-  ZipCode VARCHAR(16) NOT NULL,
-  Country VARCHAR(90) NOT NULL,
-  LOCATION AS (Street_Number + ' ' + Street_Name + ' ' + City + ', ' + State_Province_Region + ' ' + ZipCode + ' ' + Country),
+  STREET_NUMBER VARCHAR(20) NOT NULL,
+  STREET_NAME VARCHAR(50) NOT NULL,
+  CITY VARCHAR(60) NOT NULL,
+  STATE_PROVINCE_REGION VARCHAR(50) NOT NULL,
+  ZIPCODE VARCHAR(16) NOT NULL,
+  COUNTRY VARCHAR(90) NOT NULL,
   UNIQUE (NAME),
-  NumberOfEmployees              AS (dbo.GetNumberOfEmployees(ID))
+  NUMBER_OF_EMPLOYEES              AS (dbo.GetNumberOfEmployees(ID))
 );
