89d68c3048d0fe36ffc668525ff955f2e305be5c
Ian Fennen <ifennen@YMAIL.COM>
2018-04-09 01:07:50 -0500

    Added Create Departments Page.

---
+++ C#/Project_Managment_Website/Departments.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Departments.aspx.cs" Inherits="Departments" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Department Name: "></asp:Label>
+        <asp:TextBox ID="departmentNameField" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Street Number:      "></asp:Label>
+        <asp:TextBox ID="streetNumberField" runat="server" ></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Street Name: "></asp:Label>
+        <asp:TextBox ID="streetNameField" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="City: "></asp:Label>
+        <asp:TextBox ID="cityField" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="State/Province: "></asp:Label>
+        <asp:TextBox ID="stateProvinceField" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Zipcode/Postcode: "></asp:Label>
+        <asp:TextBox ID="zipcodeField" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Country: "></asp:Label>
+        <asp:TextBox ID="countryField" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="submitButton" runat="server" OnClick="submitButton_Click" Text="Submit" />
+    
+        <br />
+        <asp:Label ID="outputLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ C#/Project_Managment_Website/Departments.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Departments : System.Web.UI.Page
+{
+    /// <summary>
+    /// This method is called on page load. It will validate a user has a valid session 
+    /// and redirect them to the login page if it is not valid.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!AuthenticateSession())
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+
+    /// <summary>
+    /// Retrieves the user's information from the cookies and validates the session.
+    /// </summary>
+    /// <returns>True if a valid session. False otherwise.</returns>
+    private bool AuthenticateSession()
+    {
+        if (Request.Cookies["SessionID"] != null)
+        {
+            String sessionID = Request.Cookies["SessionID"].Value.Split('=')[1];
+            String employeeID = Request.Cookies["UserID"].Value.Split('=')[1];
+            return LoginValidator.ValidateSession(employeeID, sessionID);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected void submitButton_Click(object sender, EventArgs e)
+    {
+        String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PROJECT_MANAGMENTConnectionString"].ConnectionString;
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            String employeeID = Request.Cookies["UserID"].Value.Split('=')[1];
+
+            con.Open();
+            //Submit the hours for the employee
+            String submitStatement =
+                "INSERT INTO DEPARTMENTS (NAME, STREET_NUMBER, STREET_NAME, CITY, STATE_PROVINCE_REGION, ZIPCODE, COUNTRY) " +
+                "VALUES (@NAME, @STREET_NUMBER, @STREET_NAME, @CITY, @STATE_PROVINCE_REGION, @ZIPCODE, @COUNTRY)";
+            SqlCommand cmd = new SqlCommand(submitStatement, con);
+            SqlParameter nameParameter = new SqlParameter("@NAME", SqlDbType.VarChar, 70);
+            SqlParameter streetNumberParameter = new SqlParameter("@STREET_NUMBER", SqlDbType.VarChar, 20);
+            SqlParameter streetNameParameter = new SqlParameter("@STREET_NAME", SqlDbType.VarChar, 50);
+            SqlParameter cityParameter = new SqlParameter("@CITY", SqlDbType.VarChar, 60);
+            SqlParameter stateParameter = new SqlParameter("@STATE_PROVINCE_REGION", SqlDbType.VarChar, 50);
+            SqlParameter zipcodeParameter = new SqlParameter("@ZIPCODE", SqlDbType.VarChar, 16);
+            SqlParameter countryParameter = new SqlParameter("@COUNTRY", SqlDbType.VarChar, 90);
+
+            nameParameter.Value = departmentNameField.Text;
+            streetNumberParameter.Value = streetNumberField.Text;
+            streetNameParameter.Value = streetNameField.Text;
+            cityParameter.Value = cityField.Text;
+            stateParameter.Value = stateProvinceField.Text;
+            zipcodeParameter.Value = zipcodeField.Text;
+            countryParameter.Value = countryField.Text;
+
+
+            cmd.Parameters.Add(nameParameter);
+            cmd.Parameters.Add(streetNumberParameter);
+            cmd.Parameters.Add(streetNameParameter);
+            cmd.Parameters.Add(cityParameter);
+            cmd.Parameters.Add(stateParameter);
+            cmd.Parameters.Add(zipcodeParameter);
+            cmd.Parameters.Add(countryParameter);
+
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            outputLabel.Text += "Entry has been recorded.";
+
+
+        }
+    }
+}
