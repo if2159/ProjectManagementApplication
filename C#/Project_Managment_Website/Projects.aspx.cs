85d4144d578df4f8fd4f67a1c5439ff5e0dd583b
bryan-acuna <bryanacuna1994@icloud.com>
2018-04-17 22:16:40 -0500

    Checks non null fields are filled in

--- C#/Project_Managment_Website/Projects.aspx.cs
+++ C#/Project_Managment_Website/Projects.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -24,6 +24,7 @@ public partial class Projects : System.Web.UI.Page
         }
         startDateField.Text = DateTime.Now.ToShortDateString();
     }
+    String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PROJECT_MANAGMENTConnectionString"].ConnectionString;
 
     /// <summary>
     /// Retrieves the user's information from the cookies and validates the session.
@@ -52,49 +53,66 @@ public partial class Projects : System.Web.UI.Page
 
     protected void submitButton_Click(object sender, EventArgs e)
     {
-        String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PROJECT_MANAGMENTConnectionString"].ConnectionString;
 
         using (SqlConnection con = new SqlConnection(connectionString))
         {
-            String employeeID = Request.Cookies["UserID"].Value.Split('=')[1];
-
-            con.Open();
-            //Submit the hours for the employee
-            String submitStatement =
-                "INSERT INTO PROJECTS (NAME, BUDGET,CONTROLLING_DEPARTMENT,EID,CONTROLLING_TEAM,START_DATE,STATUS_TYPE)" +
-                "VALUES (@NAME, @BUDGET,@CONTROLLING_DEPARTMENT,@EID,@CONTROLLING_TEAM,@START_DATE,@STATUS_TYPE)";
-            SqlCommand cmd = new SqlCommand(submitStatement, con);
-            SqlParameter nameParameter = new SqlParameter("@NAME", SqlDbType.VarChar, 70);
-            SqlParameter budgetNumberParameter = new SqlParameter("@BUDGET", SqlDbType.VarChar, 20);
-            SqlParameter controllingDeparmentNumberParameter = new SqlParameter("@CONTROLLING_DEPARTMENT", SqlDbType.VarChar, 50);
-            SqlParameter eidNumberParameter = new SqlParameter("@EID", SqlDbType.VarChar, 60);
-            SqlParameter controllingTeamNumberParameter = new SqlParameter("@CONTROLLING_TEAM", SqlDbType.VarChar, 50);
-            SqlParameter startDateParameter = new SqlParameter("@START_DATE", SqlDbType.VarChar, 16);
-            SqlParameter statusTypeParameter = new SqlParameter("@STATUS_TYPE", SqlDbType.VarChar, 90);
-
-            nameParameter.Value = projectNameField.Text;
-            budgetNumberParameter.Value = budgetField.Text;
-            controllingDeparmentNumberParameter.Value = controllingDepartmentField.Text;
-            eidNumberParameter.Value = employeeIDField.Text;
-            controllingTeamNumberParameter.Value = teamsDropDownField.Text;
-            startDateParameter.Value = startDateField.Text;
-            statusTypeParameter.Value = statusTypeField.Text;
-
-
-            cmd.Parameters.Add(nameParameter);
-            cmd.Parameters.Add(budgetNumberParameter);
-            cmd.Parameters.Add(controllingDeparmentNumberParameter);
-            cmd.Parameters.Add(eidNumberParameter);
-            cmd.Parameters.Add(controllingTeamNumberParameter);
-            cmd.Parameters.Add(startDateParameter);
-            cmd.Parameters.Add(statusTypeParameter);
+            if (int.Parse(controllingDepartmentField.SelectedValue) == -1)
+            {
+                finalLabel.Text = "Please select a Department";
+            }
+            else if (int.Parse(statusTypeField.SelectedValue) == -1)
+            {
+                finalLabel.Text = "Please select a the status of Project";
+            }
             
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
-            finalLabel.Text = (nameParameter.Value + " has been created!");
-            //outputLabel.Text += "Entry has been recorded.";
+            else if (checkIfValidEmployeeID())
+            {
+                String employeeID = Request.Cookies["UserID"].Value.Split('=')[1];
+
+                con.Open();
+                //Submit the hours for the employee
+                String submitStatement =
+                    "INSERT INTO PROJECTS (NAME, BUDGET,CONTROLLING_DEPARTMENT,EID,CONTROLLING_TEAM,START_DATE,STATUS_TYPE)" +
+                    "VALUES (@NAME, @BUDGET,@CONTROLLING_DEPARTMENT,@EID,@CONTROLLING_TEAM,@START_DATE,@STATUS_TYPE)";
+                SqlCommand cmd = new SqlCommand(submitStatement, con);
+                SqlParameter nameParameter = new SqlParameter("@NAME", SqlDbType.VarChar, 70);
+                SqlParameter budgetNumberParameter = new SqlParameter("@BUDGET", SqlDbType.VarChar, 20);
+                SqlParameter controllingDeparmentNumberParameter =
+                    new SqlParameter("@CONTROLLING_DEPARTMENT", SqlDbType.VarChar, 50);
+                SqlParameter eidNumberParameter = new SqlParameter("@EID", SqlDbType.VarChar, 60);
+                SqlParameter controllingTeamNumberParameter =
+                    new SqlParameter("@CONTROLLING_TEAM", SqlDbType.VarChar, 50);
+                SqlParameter startDateParameter = new SqlParameter("@START_DATE", SqlDbType.VarChar, 16);
+                SqlParameter statusTypeParameter = new SqlParameter("@STATUS_TYPE", SqlDbType.VarChar, 90);
+
+                nameParameter.Value = projectNameField.Text;
+                budgetNumberParameter.Value = budgetField.Text;
+                controllingDeparmentNumberParameter.Value = controllingDepartmentField.Text;
+                eidNumberParameter.Value = employeeIDField.Text;
+                controllingTeamNumberParameter.Value = teamsDropDownField.Text;
+                startDateParameter.Value = startDateField.Text;
+                statusTypeParameter.Value = statusTypeField.Text;
 
 
+                cmd.Parameters.Add(nameParameter);
+                cmd.Parameters.Add(budgetNumberParameter);
+                cmd.Parameters.Add(controllingDeparmentNumberParameter);
+                cmd.Parameters.Add(eidNumberParameter);
+                cmd.Parameters.Add(controllingTeamNumberParameter);
+                cmd.Parameters.Add(startDateParameter);
+                cmd.Parameters.Add(statusTypeParameter);
+
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+                finalLabel.Text = (nameParameter.Value + " has been created!");
+                //outputLabel.Text += "Entry has been recorded.";
+
+
+            }
+            else
+            {
+                finalLabel.Text = "The Employee Id does not exist";
+            }
         }
     }
 
@@ -102,14 +120,15 @@ public partial class Projects : System.Web.UI.Page
     //{
 
     //}
+
+    //Insert a -Select- item, used to determine if user tries to submit page without proper team selected
     protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
     {
 
     }
-    //Insert a -Select- item, used to determine if user tries to submit page without proper team selected
     protected void SqlDataSouce1_DataBound(object sender, EventArgs e)
     {
-        controllingDepartmentField.Items.Insert(0, new ListItem("-Select-", String.Empty));
+        controllingDepartmentField.Items.Insert(0, new ListItem("-Select-", "-1"));
         controllingDepartmentField.SelectedIndex = 0; ;
     }
 
@@ -121,10 +140,13 @@ public partial class Projects : System.Web.UI.Page
     {
 
     }
+    
+    
     //Insert a -Select- item, used to determine if user tries to submit page without proper team selected
     protected void SqlDataSouce2_DataBound(object sender, EventArgs e)
     {
-        teamsDropDownField.Items.Insert(0, new ListItem("-Select-", String.Empty));
+        teamsDropDownField.Items.Add(new ListItem("None", String.Empty));
+        teamsDropDownField.Items.Insert(0, new ListItem("-Select-", "-1"));
         teamsDropDownField.SelectedIndex = 0; ;
     }
 
@@ -132,6 +154,19 @@ public partial class Projects : System.Web.UI.Page
     protected void teamsDropDown_SelectedIndexChanged(object sender, EventArgs e)
     {
 
+    }
+    protected void SqlDataSource3_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+
+    }
+    protected void SqlDataSource4_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+        
+    }
+    protected void SqlDataSouce4_DataBound(object sender, EventArgs e)
+    {
+        statusTypeField.Items.Insert(0, new ListItem("-Select-", "-1"));
+        statusTypeField.SelectedIndex = 0;
     }
 
 
@@ -142,6 +177,37 @@ public partial class Projects : System.Web.UI.Page
 
     protected void employeeIDField_TextChanged1(object sender, EventArgs e)
     {
-        //employeeIDField.
+        
+    }
+    protected bool checkIfValidEmployeeID()
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            String queryStatement = "SELECT U.EMPLOYEE_ID FROM USERS AS U WHERE EMPLOYEE_ID = @eid";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(queryStatement, con);
+            SqlParameter eidParameter = new SqlParameter("@eid", SqlDbType.Int);
+            if (string.IsNullOrEmpty(employeeIDField.Text))
+            {
+                return false;
+            }
+            eidParameter.Value = int.Parse(employeeIDField.Text);
+            cmd.Parameters.Add(eidParameter);
+            cmd.Prepare();
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                if (rdr.HasRows)
+                {
+                    con.Close();
+                    return true;
+
+                }
+                else
+                {
+                    con.Close();
+                    return false;
+                }
+            }
+        }
     }
 }
