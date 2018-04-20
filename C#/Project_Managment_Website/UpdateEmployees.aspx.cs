65dd4c38fd208148f419692e600d79e94ee2184d
bryan-acuna <bryanacuna1994@icloud.com>
2018-04-20 07:45:25 -0500

    <%@ Page Language="C#" AutoEventWireup="true" CodeFile="UpdateEmployees.aspx.cs" Inherits="UpdateEmployees" %>  <!DOCTYPE html>  <html xmlns="http://www.w3.org/1999/xhtml"> <head runat="server">     <title></title> </head> <body> <form id="form1" runat="server">     <div>         <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:PROJECT_MANAGMENTConnectionString %>" SelectCommand="SELECT [EMPLOYEE_ID] FROM [EMPLOYEES]" OnSelecting="SqlDataSource1_Selecting"></asp:SqlDataSource>         <asp:Label ID="Label1" runat="server" Text="Users Employee ID:"></asp:Label>         <asp:TextBox ID="employeeIDField" runat="server" DataSourceID="SqlDataSource1" DataTextField="EMPLOYEE_ID" DataValueField="EMPLOYEE_ID" OnSelectedIndexChanged="projectsDropDown_SelectedIndexChanged" AutoPostBack="true" OnDataBound ="SqlDataSouce1_DataBound">         </asp:TextBox>         <asp:RequiredFieldValidator runat="server" id="RequiredFieldValidator1" controltovalidate="employeeIDField" errormessage="Please enter Employee ID!" />         <asp:RegularExpressionValidator ID="RegularExpressionValidator2" ControlToValidate="employeeIDField" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>         <br />         <br />                    <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:PROJECT_MANAGMENTConnectionString %>" SelectCommand="SELECT [TEAM_ID] FROM [TEAMS]" OnSelecting="SqlDataSource1_Selecting"></asp:SqlDataSource>         <asp:Label ID="Label2" runat="server" Text="Team to update:"></asp:Label>         <asp:DropDownList ID="teamsDropDown" runat="server" DataSourceID="SqlDataSource2" DataTextField="TEAM_ID" DataValueField="TEAM_ID" OnDataBound="SqlDataSouce2_DataBound" AutoPostBack="true">         </asp:DropDownList>                   <br />         <br />         <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />            <br />         <asp:Label ID="FinalLabel" runat="server" Text=""></asp:Label>      </div> </form> </body> </html>

---
+++ C#/Project_Managment_Website/UpdateEmployees.aspx.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UpdateEmployees : System.Web.UI.Page
+{
+    String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PROJECT_MANAGMENTConnectionString"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!AuthenticateSession())
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+
+
+    protected void statusDropDown_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+    private bool AuthenticateSession()
+    {
+        if (Request.Cookies["SessionID"] != null)
+        {
+            String sessionID = Request.Cookies["SessionID"].Value.Split('=')[1];
+            String employeeID = Request.Cookies["UserID"].Value.Split('=')[1];
+            return LoginValidator.ValidateSession(employeeID, sessionID);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        if (checkIfValidEmployeeID())
+        {
+            if (int.Parse(teamsDropDown.SelectedValue) == -1)
+            {
+                FinalLabel.Text = "Please select a Team";
+            }
+            else
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    String queryStatement = "UPDATE EMPLOYEES SET TEAM_ID = @TeamID WHERE EMPLOYEE_ID = @EMPLOYEE_ID";
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(queryStatement, con);
+                    SqlParameter EmployeeIDParameter = new SqlParameter("@EMPLOYEE_ID", SqlDbType.Int);
+                    SqlParameter TEAMIDParameter = new SqlParameter("@TeamID", SqlDbType.Int);
+                    
+
+                    EmployeeIDParameter.Value = int.Parse(employeeIDField.Text);
+                    TEAMIDParameter.Value = int.Parse(teamsDropDown.SelectedValue);
+
+
+                    cmd.Parameters.Add(EmployeeIDParameter);
+                    cmd.Parameters.Add(TEAMIDParameter);
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+                    FinalLabel.Text = ("Team has been updated!");
+                }
+            }
+        }
+        else
+        {
+            FinalLabel.Text = "This Employee ID does not exist";
+        }
+
+
+    }
+    protected void SqlDataSouce2_DataBound(object sender, EventArgs e)
+    {
+
+        teamsDropDown.Items.Insert(0, new ListItem("-Select-", "-1"));
+        teamsDropDown.SelectedIndex = 0; ;
+    }
+    protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+
+    }
+    protected bool checkIfValidEmployeeID()
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            String queryStatement = "SELECT U.EMPLOYEE_ID FROM USERS AS U WHERE EMPLOYEE_ID = @eid";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(queryStatement, con);
+            SqlParameter eidParameter = new SqlParameter("@eid", SqlDbType.Int);
+            if (string.IsNullOrEmpty(employeeIDField.Text))
+            {
+                return false;
+            }
+            eidParameter.Value = int.Parse(employeeIDField.Text);
+            cmd.Parameters.Add(eidParameter);
+            cmd.Prepare();
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                if (rdr.HasRows)
+                {
+                    con.Close();
+                    return true;
+
+                }
+                else
+                {
+                    con.Close();
+                    return false;
+                }
+            }
+        }
+    }
+}
